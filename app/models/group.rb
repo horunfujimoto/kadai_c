c9005f766cfa6988f241607b361b7768de3eb7ca
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-09-10 06:03:11 +0000

    [Add]応用課題8c:グループへの参加・退会

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,8 +7,18 @@ class Group < ApplicationRecord
   validates :introduction, presence: true
   has_one_attached :group_image
 
+  has_many :users, through: :group_users, source: :user
+
+  def include_user?(user)
+    group_users.exists?(user_id: user.id)
+  end
+
   def get_group_image(width, height)
-    (group_image.attached?) ? group_image : 'no_image.jpg'
+  unless group_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      group_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    group_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def is_owned_by?(user)
