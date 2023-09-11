3d397607a5486c63c362700872e064532fb6c0e0
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-09-11 09:51:11 +0000

    [Add]応用課題9c:メール配信

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  #application_mailerには全メーラー共通の設定を、sample_mailerにはメーラー個別の設定を記述
   default from: 'from@example.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -75,5 +75,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "af6cf9a4282042ffaa7e7e65465701e2.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com', #メールを相手のメールサーバーまで届ける仕組みが「SMTP」
+   port:                  587,
+   domain:               'gmail.com',
+   user_name:            'hello.horurun@gmail.com',
+   password:             'xqcl wfhy ravm ekne',
+   authentication:       'login',
+   enable_starttls_auto:  true
+  }
 
 end
